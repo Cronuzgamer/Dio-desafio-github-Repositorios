d0881fad446ad7d56e2b1cfc55acaa380296dd9c
CronuzGamer <gabrieljknd@gmail.com>
2022-07-23 03:09:59 -0300

    update

---
+++ Introduçao ao JavaScript/JavaScript/teste.js
@@ -0,0 +1,29 @@
+// //var preco = 2;
+// var desconto = 0,2;
+// const PRECO = 2;
+// var total = PRECO - desconto;
+// PRECO = PRECO - desconto;
+
+
+// function soma(a,b) {
+//     return a + b; // 8
+// }
+// soma(3, 5);
+
+
+// console.log ('Hello world!');// Manda o console imprimir o texto
+
+// function returnEvenEvalues (array){
+//     let evenNums [];
+//     for(let i = 0; i < array.lenght)
+//     if(array[i] % 2 === 0){
+//         evenNums.push(array[i]);
+
+//     }
+// }
+// console.log('os numeros pares são: 'evenNums);
+
+// let array = [1,2,4,5,7,8];
+// returnEvenEvalues(array);
+
+
